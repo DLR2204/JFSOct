3051713eff754d8f3cecaeab8da42051a78eea62
DLR2204 <n130488@rguktn.ac.in>
2024-11-04 22:28:02 +0530

    Add files via upload

---
+++ Corejava/intro/Hello.java
@@ -0,0 +1,22 @@
+package intro;
+
+public class Hello{
+	
+	public static void main(String[] args) {
+		
+		Hello obj = new Hello();
+		
+		System.out.println(obj.equals(obj));
+		
+		System.out.println(obj.toString());
+		
+	
+		
+	}
+
+}
+
+
+//For every java class the parent class is Object.
+
+//For every java class the default package is java.lang;
