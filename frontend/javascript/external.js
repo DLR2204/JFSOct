08aed11ff667a21b3ab3ccb2a1f585fb0e94b15d
DLR2204 <n130488@rguktn.ac.in>
2024-12-11 21:32:07 +0530

    Add files via upload

---
+++ frontend/javascript/external.js
@@ -0,0 +1,10 @@
+function check(){
+
+window.alert("Iam from the external js file")
+
+document.write("Iam from the document write function")
+
+console.log("Iam from the console log function")
+
+
+}
